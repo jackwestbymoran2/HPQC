0a0489ca401238ff7df9c8637c8d1373cb665da9
Jack Westbymoran <jack.westbymoran2@mail.dcu.ie>
2026-02-26 15:52:12 +0000

    Added MPI week3 program

--- week3.c
+++ week3.c
@@ -1,2 +1,17 @@
-print("hello world")
+#include <stdio.h>
+#include <mpi.h>
 
+int main(int argc, char *argv[]) {
+
+    int rank;
+    int size;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    printf("Hello World from process %d out of %d processes\n", rank, size);
+
+    MPI_Finalize();
+    return 0;
+}
